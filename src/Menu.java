33def7f834bffeb40d6c9f482b6ec8291e893cda
bezo97 <bezozo97@gmail.com>
2017-03-19 17:51:31 +0100

    menu
    -zotya

--- src/Menu.java
+++ src/Menu.java
@@ -1,20 +1,48 @@
-public class Menu {
-	private int progress;
+import java.util.Scanner;
+
+public class Menu {//menupontok itt
+	private int progress;//hanyadik palya
 	private String palya;
-	public void start(String palya) {
+	public void start(String ujpalya) 
+	{//uj Jatek
+		System.out.println("Menu.start()");
+		
+		//skeleton dolgok
+		System.out.println("Adja meg a palya nevet: ");
+		Scanner reader = new Scanner(System.in);
+		String valasz = reader.next();
+		reader.close();
+		palya=valasz;//ujpalya
+		
+		Jatek.getInstance().start(palya);//uj jatek ezzel a palyaval
 	}
 	
-	public void save() {
+	public void save() 
+	{//progresst menti
+		System.out.println("Menu.save()");
+		
+		//progress-t kimenteni fajlba
+		
 	}
 	
-	public int select() {
-		return 0;
+	public int select() 
+	{//palya szamat adja vissza
+		System.out.println("Menu.select()");		
+		return progress;
 	}
 	
-	public int load() {
-		return 0;
+	public int load() 
+	{//meddig jutottunk el
+		System.out.println("Menu.load()");
+		
+		//progress-t betolti fajlbol
+		
+		return progress;
 	}
 	
-	public void exit() {
+	public void exit() 
+	{//kilep
+		System.out.println("Menu.exit()");
+		
 	}
 }
