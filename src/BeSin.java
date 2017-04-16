34b871a92e830a7c2325215da9128b1f1743e152
bezo97 <bezozo97@gmail.com>
2017-04-16 22:57:35 +0200

    Zotya BeSin kommentek kesz
    + atneveztem egy parametert mert megteveszto volt

--- src/BeSin.java
+++ src/BeSin.java
@@ -1,57 +1,100 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class BeSin extends SinElem {//vonatok belelepesi pontja
-	List<Vonat> beadando = new ArrayList<>();
+/**
+ * @author Zotya
+ * BeSin osztaly, SinElem leszarmazottja.
+ * Vonatok belepesi pontja
+ * csak egy kimeno aga van, mert a palya szelen van
+ */
+public class BeSin extends SinElem {
+	List<Vonat> beadando = new ArrayList<>();//taroljuk a vonatokat amig varnak
 	
-	
-	
+	/**
+	 * @author Jana (+Zotya kommentek)
+	 * Beadando vonatokat ezekben az osztalyokban taroljuk amig varnak
+	 */
 	class Vonat
 	{
 		public Integer tck;
 		public String id;
-		public String szerelvenyek;
+		public String vagonok;
 		
-		Vonat(String id, Integer tck, String szerelvenyek) {
+		/**
+		 * konstruktor
+		 * @param id Azonosito
+		 * @param tck hanyadik ticknel lep a palyara
+		 * @param vagonok vagonok tipusai, pl.: x-szenes, p-piros utas
+		 */
+		Vonat(String id, Integer tck, String vagonok) {
 			this.id = id;
-			this.szerelvenyek = szerelvenyek;
+			this.vagonok = vagonok;
 			this.tck = tck;
 		}
 	}
+	
+	/**
+	 * konstruktor
+	 * @param id Azonosito
+	 */
 	BeSin(String id) 
     {
     	super(id);
     }
-	@Override //sinEleme
+	
+	/**
+	 * szamolja csak hogy raleptek
+	 * @param m melyik mozdony
+	 * @param s honnan
+	 */
+	@Override
 	public void leptet(Mozdony m, SinElem s) {
 		//A mozdony ralepett a sinElemre
         szerelvenyek++;//SinEleme
 	}
 	
-	
+	/**
+	 * Visszaadja a kovetkezo SinElemet az elozo alapjan
+	 * @param elozo Honnan jott a vonat. Mivel ez besin, elvileg null a param
+	 */
 	@Override
-	public SinElem getKovSinElem(SinElem elozo) {//itt az elozo elvileg null
+	public SinElem getKovSinElem(SinElem elozo) {
 		return sinA;//ez jo, mert nincs sinBje a BeSinnek
 	}
-	public void VonatBead(String azon, int tck, String vonatok) {
-		beadando.add(new Vonat(azon, tck, vonatok));
+	
+	/**
+	 * Uj vonat hozzaadasa, terepasztal hivja meg
+	 * @param azon Azonositoja a vonatnak
+	 * @param tck Hanyadik ticknel indul
+	 * @param vagonok Vagonok tipusai, pl.: x-szenes, p-piros utas
+	 */
+	public void VonatBead(String azon, int tck, String vagonok) {
+		beadando.add(new Vonat(azon, tck, vagonok));
 	}
 	
 	
-	
+	/**
+	 * Attributumok lekerdezesere
+	 * @param attr Milyen attributumot kerdezunk le
+	 */
 	@Override
 	public void GetInfo(String attr) {
 		super.GetInfo(attr);
 		
 	}
+	
+	/**
+	 * Terepasztal hivja meg, bead vonatot ha indul ebben a tickben
+	 * @param t Hanyadik ticknel jarunk
+	 */
 	public void tick(int t) {
 		for(Vonat v: beadando) {
 			if (v.tck == t) {
 				Vagon kovKocsi = null;
 				List<Vagon> vonat = new ArrayList<>();
-				for(int i = v.szerelvenyek.length()-1; i >= 0; i--) {
+				for(int i = v.vagonok.length()-1; i >= 0; i--) {//vonat felepitese hatulrol kezdve
 					Vagon u = null;
-					switch (v.szerelvenyek.charAt(i)) {
+					switch (v.vagonok.charAt(i)) {//mi a tipusa a vagonnak
 					case 'p':
 						u = new UtasKocsi(v.id+"-u"+(i+1), kovKocsi, Szin.PIROS);
 						break;
@@ -67,7 +110,6 @@ public class BeSin extends SinElem {//vonatok belelepesi pontja
 					case 'x':
 						u = new SzenesKocsi(v.id+"-s"+(i+1), kovKocsi);
 						break;
-
 					default:
 						break;
 					}
@@ -75,11 +117,9 @@ public class BeSin extends SinElem {//vonatok belelepesi pontja
 					vonat.add(u);
 					kovKocsi = u;
 				}
-				Mozdony m = new Mozdony(v.id, kovKocsi, this);
-				Terepasztal.getInstance().AddMozdony(m);
+				Mozdony m = new Mozdony(v.id, kovKocsi, this);//vegen az egeszet egy mozdonyra kapcsoljuk
+				Terepasztal.getInstance().AddMozdony(m);//elinditjuk itt
 			}
 		}
 	}
 }
-
-//Zotya
