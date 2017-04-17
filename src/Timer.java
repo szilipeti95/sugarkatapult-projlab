1235ac8d44b28a81be0d4fb94d667979c9a699dc
bezo97 <bezozo97@gmail.com>
2017-04-18 01:02:24 +0200

    megint..

--- src/Timer.java
+++ src/Timer.java
@@ -1,6 +1,6 @@
 public class Timer {
         //elindul a Timer
-	//Akkor hívódik meg amikor a játékos elindítja a játékot.
+	//Akkor hivodik meg amikor a jatekos elinditja a jatekot.
 	public void start() {
 	}
 }
